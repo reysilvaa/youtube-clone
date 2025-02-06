82c7852c76a06bc584ee3b056dc51c6e2233c612
reysilvaa <reynaldsilva123@gmail.com>
2025-02-06 20:23:42 +0700

    vercel test1

--- src/app/components/VideoGrid/VideoGrid.tsx
+++ src/app/components/VideoGrid/VideoGrid.tsx
@@ -6,33 +6,41 @@ import { VideoCard } from "../VideoCard/VideoCard";
 export const VideoGrid = () => {
   const videos = [
     {
-      thumbnail: '/thumbnail.png',
+      thumbnail: '/rey.jpg',
       // thumbnail: 'https://c4.ttcache.com/thumbnail/hcMKnU0Eoz3/288x162/513c217f544fd9715d5e915944c50fb6.28.jpg',
-      title: 'Building a YouTube Clone with Next.js and Tailwind CSS',
+      title: 'VIRAL!!!! PRIA INI CABULI JANDA USIA 50 TAHUN DI KAMAR MANDI',
       channel: 'Code Tutorial',
       views: '100K views',
       timestamp: '2 days ago',
       profile_image: '/profile.png', // Profile image for the video
     },
     {
-      thumbnail: '/thumbnail.png',
-      title: 'Mastering Tailwind CSS in 30 Minutes',
+      thumbnail: '/thumb2.jpg',
+      title: 'BOKEP GAY GAYA DOGGY STYLE',
       channel: 'Dev Academy',
       views: '50K views',
       timestamp: '1 week ago',
       profile_image: '/profile.png', // Profile image for the video
     },
     {
-      thumbnail: '/thumbnail.png',
-      title: 'Understanding Next.js API Routes',
+      thumbnail: '/thumb1.jpg',
+      title: 'BOKEP GAY KELUAR 33 KALI ADUHH DEKK',
       channel: 'JavaScript Daily',
       views: '200K views',
       timestamp: '5 days ago',
       profile_image: '/profile.png', // Profile image for the video
     },
     {
-      thumbnail: '/thumbnail.png',
-      title: 'Learn React in 30 Minutes',
+      thumbnail: '/thumb3.png',
+      title: 'BOKEP PRIA MBEROT PAKISAJI',
+      channel: 'React Tutorials',
+      views: '300K views',
+      timestamp: '3 days ago',
+      profile_image: '/profile.png', // Profile image for the video
+    },
+    {
+      thumbnail: '/thumb4.png',
+      title: 'Pria Merokok Squirt  Keenakan sampai Kelojotan',
       channel: 'React Tutorials',
       views: '300K views',
       timestamp: '3 days ago',
